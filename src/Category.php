16784aa57b4bb28e20de6da651851bde5a60dbbb
Abraham Yusuf <aaondowasey@gmail.com>
2017-12-02 18:55:32 +0100

    defined foreign key relationships on the models.

--- src/Category.php
+++ src/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * Get all tasks of this category.
+     */
+    public function tasks()
+    {
+        return $this->hasMany('abrahamy\Todo\Task');
+    }
 }
--- src/Task.php
+++ src/Task.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    /**
+     * Get the category of a task.
+     */
+    public function category()
+    {
+        return $this->belongsTo('abrahamy\Todo\Category');
+    }
 }
