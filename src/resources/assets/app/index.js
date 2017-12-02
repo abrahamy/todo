895a75ef16da05458ca070ac5b6a58f35cc00e90
Abraham Yusuf <aaondowasey@gmail.com>
2017-12-02 17:21:20 +0100

    refactoring and bug fixes

--- src/resources/assets/app/index.js
+++ src/resources/assets/app/index.js
@@ -1,10 +1,30 @@
+import axios from 'axios'
+import Buefy from 'buefy'
 import Vue from 'vue'
-import components from './components'
-import plugin from './plugin'
+import TasksComponent from './components/Tasks.vue'
 
-Vue.use(plugin)
+if (process.env.NODE_ENV !== 'production') {
+    Vue.config.debug = true
+    Vue.config.devtools = true
+}
+
+Vue.use(Buefy, { defaultIconPack: 'fa' })
+
+document.addEventListener('DOMContentLoaded', event => {
+    const http = axios.create({
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json',
+        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
+      }
+    })
+
+    Object.defineProperty(Vue.prototype, '$axios', { value: http })
+})
 
 const app = new Vue({
   el: '#app',
-  components
+  components: {
+      tasks: TasksComponent
+  }
 })
